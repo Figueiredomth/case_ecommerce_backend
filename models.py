2a1daf38f554070c34c0c1ed672fbfc8d0ea811e
Figueiredomth <matheusfiigueiredo@gmail.com>
2024-09-09 14:35:20 -0300

    Adding new tests and updating the code

--- models.py
+++ models.py
@@ -25,7 +25,7 @@ class Product(db.Model):
     __tablename__ = 'products'
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False, unique=True)
     description = db.Column(db.String(150), nullable = False)
     price = db.Column(db.Float, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
